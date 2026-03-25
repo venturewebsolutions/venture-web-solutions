5262f14ba0448f49cca23c0d42cb3c0d19bdcb4b
Jordan Walker <walker.jlg@gmail.com>
2026-03-25 19:25:34 +0000

    Refactor

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,8 +2,6 @@ import type { DataEntryMap, InferEntrySchema } from 'astro:content'
 
 export type HeadingTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
-export type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6
-
 export type CollectionItem<C extends keyof DataEntryMap> = {
   id: string
 } & InferEntrySchema<C>

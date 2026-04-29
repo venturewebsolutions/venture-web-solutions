1a60b34ab09e8a6b69f55ade4c960ae59b190b12
Jordan Walker <walker.jlg@gmail.com>
2026-04-29 17:37:04 +0100

    project planner image

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,3 +1,5 @@
+import range from 'lodash/range'
+
 export function onImagesLoaded(
   images: ArrayLike<HTMLImageElement>,
   cb: () => any,
@@ -21,4 +23,50 @@ export function onImagesLoaded(
       })
     }
   }
+}
+
+const LIGHTHOUSE_MOBILE_DPR = 1.75
+const LIGHTHOUSE_DESKTOP_DPR = 1
+const DEFAULT_STEP = 250
+
+export type ImageWidths = {
+  min: number
+  max: number
+  mobile?: number
+  desktop?: number
+  step?: number
+}
+
+export type ImageSizes = string | string[]
+
+export function getWidths(widths: ImageWidths) {
+  const { min, max, mobile, desktop, step = DEFAULT_STEP } = widths
+
+  const result = [...range(min, max * 2, step), max * 2]
+
+  if (mobile) {
+    result.push(Math.ceil(mobile * LIGHTHOUSE_MOBILE_DPR))
+  }
+
+  if (desktop) {
+    result.push(Math.ceil(desktop * LIGHTHOUSE_DESKTOP_DPR))
+  }
+
+  return [...new Set(result)].toSorted((a, b) => a - b)
+}
+
+export function getSizes(sizes: ImageSizes, widths: ImageWidths) {
+  const { mobile, desktop } = widths
+
+  const result = Array.isArray(sizes) ? [...sizes] : [sizes]
+
+  if (desktop) {
+    result.unshift(`(width: 1350px) ${widths.desktop}px`)
+  }
+
+  if (mobile) {
+    result.unshift(`(width: 412px) ${widths.mobile}px`)
+  }
+
+  return result.join(', ')
 }
